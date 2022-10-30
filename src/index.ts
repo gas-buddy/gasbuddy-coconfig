e1bf8f1b381812ae44c714da5a8dd901e4530704
Max Metral <max@pyralis.com>
2022-10-30 15:41:29 -0400

    Add const to maybe help the compiler figure out key value types

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,6 @@ const config: CoConfig = {
   'jest.config.js': { configuration: jestConfig },
   '.prettierrc.js': { configuration: prettierConfig },
   '.eslintrc.js': { configuration: eslintRc },
-};
+} as const;
 
 export default config;
