35fab679e27d8e1b9ac97942509d1a373fa2eec8
Max Metral <max@pyralis.com>
2022-10-30 15:50:06 -0400

    Strongly type export

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import fs from 'fs';
 import path from 'path';
-import type { CoConfig } from 'coconfig';
+import type { CoConfigPassthroughEntry } from 'coconfig';
 import { tsconfig, tsconfigBuild } from './tsconfig';
 import { jestConfig } from './jest';
 import { prettierConfig } from './prettier';
 import { eslintRc } from './eslint';
 
-const config: CoConfig = {
+interface GbCoConfig {
+  '.eslintignore': string,
+  '.npmignore': string,
+  'tsconfig.json': CoConfigPassthroughEntry,
+  'tsconfig.build.json': CoConfigPassthroughEntry,
+  'jest.config.js': CoConfigPassthroughEntry,
+  '.prettierrc.js': CoConfigPassthroughEntry,
+  '.eslintrc.js': CoConfigPassthroughEntry,
+}
+
+const config: GbCoConfig = {
   '.eslintignore': fs.readFileSync(path.resolve(__dirname, '../templates/eslintignore'), 'utf8'),
   '.npmignore': fs.readFileSync(path.resolve(__dirname, '../templates/npmignore'), 'utf8'),
   'tsconfig.json': { configuration: tsconfig, stringify: true },
@@ -14,6 +24,6 @@ const config: CoConfig = {
   'jest.config.js': { configuration: jestConfig },
   '.prettierrc.js': { configuration: prettierConfig },
   '.eslintrc.js': { configuration: eslintRc },
-} as const;
+};
 
 export default config;
