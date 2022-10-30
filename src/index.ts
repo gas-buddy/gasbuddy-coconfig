4606db3a4340b96d9fa8c41449385829b04e6831
Max Metral <max@pyralis.com>
2022-10-29 23:18:36 -0400

    Type safety in config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import fs from 'fs';
 import path from 'path';
+import type { CoConfig } from 'coconfig';
 import { tsconfig, tsconfigBuild } from './tsconfig';
 import { jestConfig } from './jest';
 import { prettierConfig } from './prettier';
 import { eslintRc } from './eslint';
 
-export default {
+const config: CoConfig = {
   '.eslintignore': fs.readFileSync(path.resolve(__dirname, '../templates/eslintignore'), 'utf8'),
   '.npmignore': fs.readFileSync(path.resolve(__dirname, '../templates/npmignore'), 'utf8'),
   'tsconfig.json': { configuration: tsconfig, stringify: true },
@@ -14,3 +15,5 @@ export default {
   '.prettierrc.js': { configuration: prettierConfig },
   '.eslintrc.js': { configuration: eslintRc },
 };
+
+export default config;
